0925aa328572ced5ec3a49ce4ab8889e29d2af97
4284_arib_aqil_muhammad <127897006+aribaqil@users.noreply.github.com>
2023-06-14 03:09:40 +0700

    Add files via upload

---
+++ src/components/Hero.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="jumbotron text-center">
+    <div>
+      <img src="../assets/1.3.jpg" class="img-fluid" alt="Responsive image" />
+    </div>
+    <h1 class="display-4 ">Hello, world!</h1>
+    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"></svg>
+  </section>
+</template>
+<script setup></script>
